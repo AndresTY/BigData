8f1395af72be16abd01de3780da17e46ad6f2bdc
Andres Ducuara <65153399+AndresTY@users.noreply.github.com>
2022-09-08 17:03:38 -0500

    Update function.py

--- Parcial1/to_csv/function.py
+++ Parcial1/to_csv/function.py
@@ -1,8 +1,9 @@
 import json
 
+#convert responde in csv
 def data_to_csv(data):
     info = ""
-    data = json.loads(data)
+    data = json.loads(data) #load string as json
     for i in data:
-        info += str(i[0]) + '\t' + str(i[1]) + '\n'
+        info += str(i[0]) + '\t' + str(i[1]) + '\n' #csv format
     return info
