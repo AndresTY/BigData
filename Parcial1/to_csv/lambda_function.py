7ccffc8694f63c0b76f3d166941eeababcc79331
Andres Ducuara <65153399+AndresTY@users.noreply.github.com>
2022-09-08 18:42:09 -0500

    Update lambda_function.py

--- Parcial1/to_csv/lambda_function.py
+++ Parcial1/to_csv/lambda_function.py
@@ -1,9 +1,13 @@
+#Importaciones
 import json
 import boto3
 import function
 
+#configuracion del bucket
 S3_BUCKET = 'dolar-raw-ducuara'
 s3 = boto3.client('s3')
+
+#carga de archivos
 def upload_file(file_name, bucket, object_name=None):
   if object_name is None:
     object_name = os.path.basename(file_name)
@@ -13,13 +17,13 @@ def upload_file(file_name, bucket, object_name=None):
       logging.error(e)
       return False
     return True   
-  
-  def lambda_handler(event, context):
-    object_key = "dolar-timestamp.txt"
-    file_content = s3.get_object(Bucket=S3_BUCKET, Key=object_key)["Body"].read()
-    with open('/tmp/data_raw.csv','w+') as f:
-      f.write(function.data_to_csv(file_content))
-      f.close()
-    with open("/tmp/data_raw.csv", "rb") as f:
-      s3.upload_fileobj(f, "dolar-processed-ducuara", "dolar-timestamp.csv")
-    return {'statusCode': 200,'body': 'check'}
+
+def lambda_handler(event, context):
+  object_key = "dolar-timestamp.txt" #objeto a llamar
+  file_content = s3.get_object(Bucket=S3_BUCKET, Key=object_key)["Body"].read() #obtencion del archivo
+  with open('/tmp/data_raw.csv','w+') as f:
+    f.write(function.data_to_csv(file_content)) #escritura de los datos transformados
+    f.close()
+  with open("/tmp/data_raw.csv", "rb") as f:
+    s3.upload_fileobj(f, "dolar-processed-ducuara", "dolar-timestamp.csv") #caarge archivo CSV
+  return {'statusCode': 200,'body': 'check'}
