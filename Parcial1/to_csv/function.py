97dd13bc148f8e619d27fa441eb075f8ce0de1fa
Andres Ducuara <65153399+AndresTY@users.noreply.github.com>
2022-09-12 22:50:05 -0500

    Update function.py

--- Parcial1/to_csv/function.py
+++ Parcial1/to_csv/function.py
@@ -1,9 +1,11 @@
 import json
+import time
+from datetime import datetime
 
-#convert responde in csv
 def data_to_csv(data):
-    info = ""
-    data = json.loads(data) #load string as json
+    info =""
+    data = json.loads(data)
     for i in data:
-        info += str(i[0]) + '\t' + str(i[1]) + '\n' #csv format
+        info += i[0] +'\t'+str(datetime.utcfromtimestamp(int(i[0])/1000).strftime('%Y-%m-%d %H:%M:%S'))+'\t'+ i[1]+'\n'
+        
     return info
